fbaf90a9b4aee797534fa879099dd4c7511d3553
Neelesh Bisht <neeleshbisht99@gmail.com>
2019-12-01 21:21:15 +0530

    Updates

---
+++ precache-manifest.930106ecd772da1c3ea7dc5f648dda91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "013360c776adb6094cf1d8118232d94f",
+    "url": "/interview-RoaDo/index.html"
+  },
+  {
+    "revision": "0207c05726205af176d2",
+    "url": "/interview-RoaDo/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "96b7e5474cbf6e2c8bc9",
+    "url": "/interview-RoaDo/static/js/2.aef33d5a.chunk.js"
+  },
+  {
+    "revision": "0207c05726205af176d2",
+    "url": "/interview-RoaDo/static/js/main.b1f0e0cc.chunk.js"
+  },
+  {
+    "revision": "b03f7c839aa37e77c88b",
+    "url": "/interview-RoaDo/static/js/runtime-main.13e6bc91.js"
+  },
+  {
+    "revision": "8db7ae2f1444e57cacd16d1c3d8626be",
+    "url": "/interview-RoaDo/static/media/cross.8db7ae2f.svg"
+  },
+  {
+    "revision": "c0cbd0847616c5b88e858a5e2abacdbe",
+    "url": "/interview-RoaDo/static/media/tick.c0cbd084.svg"
+  }
+]);
